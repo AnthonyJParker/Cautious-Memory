9caf082374f2d41726e319dc6a683bf70d4d1712
AnthonyJParker <candaparker@yahoo.com.au>
2020-05-11 17:40:26 +1000

    Update ESP32_website_bme280V3.ino

--- ESP32 dev/ESP32_website_bme280V3/ESP32_website_bme280V3.ino
+++ ESP32 dev/ESP32_website_bme280V3/ESP32_website_bme280V3.ino
@@ -1,15 +1,13 @@
 /*
-  WORKING AP 2020
+  WORKING AP MAY 2020
   Based on:
   https://randomnerdtutorials.com/cloud-weather-station-esp32-esp8266/#more-92139
-
-     reads a setup file on the SD card "setup.js"
-       
+     reads a setup file on the SD card "setup.js"    
      ****** On upload, press and hold BOOT button till the bootloader is started
        
         ESP32  micro SD
         GND     GND
-        +3.3v   Vcc
+        +3.3v   Vcc (NOTE: microSD adapter uses 5v)
         D19     MISO
         D23     MOSI
         D18     SCK
@@ -25,7 +23,6 @@
    #include "SPI.h"
 
    #include <WiFiMulti.h>
-   //#include "WiFi.h"
    #include <WiFiUdp.h>
    #include "ESPAsyncWebServer.h"
    #include <ArduinoJson.h>
@@ -48,17 +45,18 @@
       WifiNetwork net[10];
     };
     
-   WifiConfig wifi_conf;
+    WifiConfig wifi_conf;
    
     int wifi_net_no = 0;
-    
-    char filename1[20] = "/setup.js";           // NOTE: SD card "SETUP.JS" filename
-    void ReadSetupFile(char filename[20]);
+	
+    const char *filename1 = "/setup.js";           // NOTE: SD card "SETUP.JS" filename
+    void ReadSetupFile(const char *filename,WifiConfig &wifi_conf );
     void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
 
+    const char compile_date[] = __DATE__ " " __TIME__;
+
     WiFiMulti wifiMulti;
-    // Create AsyncWebServer object on port 80
-    AsyncWebServer server(80);
+    AsyncWebServer server(80);               // Create AsyncWebServer object on port 80
     
     File myFile;    //myFile (setup.js)
   
@@ -68,10 +66,13 @@
     // Keep this API Key value to be compatible with the PHP code provided in the project page.
     // If you change the apiKeyValue value, the PHP file /esp-post-data.php also needs to have the same key
     String apiKeyValue = "tPmAT5Ab3j7F9";
-    String sensorName = "BME280";
-    String sensorLocation = "Bullarto South";
     
-    char sensLocatArray[5][40];                         // added Station Location array ***************
+    String sensorName = "BME280";
+    String sensorLocation = "Fafa Island";
+    String sensorHome = "deviceSpot";                   // for the http POST data to SQL
+    char sensLocatArray[5][40];                         // added Station SSID, PSWRD and Location array
+    char sensSSIDArray[5][40];
+    char sensPSWRDArray[5][40];
     
     #define SEALEVELPRESSURE_HPA (1013.25)
     
@@ -85,71 +86,86 @@
     // Set timer to 30 seconds (30000)
     //unsigned long timerDelay = 30000;
 
-void setup() {
-    Serial.begin(115200);
-    delay(100);
-    // (you can also pass in a Wire library object like &Wire2)
-    bool status;
-    status = bme.begin(0x76);
-    if (!status) {
-      Serial.println("Could not find a valid BME280 sensor, check wiring or change I2C address!");
-      while (1);
-    }
+	void setup() {
+		Serial.begin(115200);
+		bool status;
+		status = bme.begin(0x76);
+		if (!status) {
+		  Serial.println("Could not find a valid BME280 sensor, check wiring or change I2C address!");
+		  while (1);
+		}
 
-    for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 5; i++) {
+    strcpy(sensSSIDArray[i],"123456789");
+    strcpy(sensPSWRDArray[i],"password");
     strcpy(sensLocatArray[i],"Location 1"); // put text in the location array
-    }
-    
-  //SD card setup
-   if (!SD.begin()) {
-        Serial.println("Card Mount Failed");
-        return;
-      }
-      uint8_t cardType = SD.cardType();
-    
-      if (cardType == CARD_NONE) {
-        Serial.println("No SD card attached");
-        return;
-      }
-    
-      Serial.print("SD Card Type: ");
-      if (cardType == CARD_MMC) {
-        Serial.println("MMC");
-      } else if (cardType == CARD_SD) {
-        Serial.println("SDSC");
-      } else if (cardType == CARD_SDHC) {
-        Serial.println("SDHC");
-      } else {
-        Serial.println("UNKNOWN");
-      }
-    
-      uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-      Serial.printf("SD Card Size: %lluMB\n", cardSize);
-    
-      listDir(SD, "/", 0);
+		}
+		
+	  //SD card setup
+	   if (!SD.begin()) {
+			Serial.println(" ");
+			Serial.println("***** Card Mount Failed *****");
+			return;
+		  }
+		  uint8_t cardType = SD.cardType();
+		
+		  if (cardType == CARD_NONE) {
+			Serial.println("No SD card attached");
+			return;
+		  }
+		
+		  Serial.print("SD Card Type: ");
+		  if (cardType == CARD_MMC) {
+			Serial.println("MMC");
+		  } else if (cardType == CARD_SD) {
+			Serial.println("SDSC");
+		  } else if (cardType == CARD_SDHC) {
+			Serial.println("SDHC");
+		  } else {
+			Serial.println("UNKNOWN");
+		  }
+		
+		  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+		  Serial.printf("SD Card Size: %llu MB\n", cardSize);
+		
+		  listDir(SD, "/", 0);
 
-      Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-      Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
-    
-      ReadSetupFile(filename1);
-      delay(200);
-      
-      uint8_t stat = wifiMulti.run();
-      while (stat != WL_CONNECTED) {
-            delay(500);
-            Serial.print(".");
-        }
-      if (stat == WL_CONNECTED) {
-            Serial.println("");
-            Serial.println("WiFi connected");
-            Serial.println("IP address: ");
-            Serial.println(WiFi.localIP());
-       }
- 
-      Serial.print("Timer set to ");
-      Serial.print(timerDelay / 1000);
-      Serial.println(" seconds (timerDelay variable), it will take this many seconds before publishing the first reading.");
-    }
+		  Serial.printf("Total space: %llu MB\n", SD.totalBytes() / (1024 * 1024));
+		  Serial.printf("Used space: %llu MB\n", SD.usedBytes() / (1024 * 1024));
+		
+		  ReadSetupFile(filename1, wifi_conf); // read "/setup.js" on the SD card and save wifi details
+		  
+		  uint8_t stat = wifiMulti.run();
+		  while (stat != WL_CONNECTED) {
+				delay(500);
+				Serial.print(".");
+			}
+		  if (stat == WL_CONNECTED) {
+				Serial.println("");
+        Serial.print("WiFi connected to: ");
+        Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+		   }
+		         
+      for (int i = 0; i < 5; i++) {
+        Serial.print("checking device location.....");
+        Serial.println(i);
+            //if( WiFi.SSID() == sensSSIDArray[i] ) {
+            if(strcmp(WiFi.SSID().c_str(), sensSSIDArray[i] ) == 0)  { // check which wifi we connected to and get site location
+            Serial.print("Device location is: ");
+            Serial.println(sensLocatArray[i]);
+            sensorHome = sensLocatArray[i]; // Get actual sensor location/wifi network for the http POST data
+          }
+      }
+       
+		  Serial.print("ESP32_website_bme280V3.ino programmed on: ");
+		  Serial.println(compile_date);
+		  Serial.print("Timer set to ");
+		  Serial.print(timerDelay / 1000);
+		  Serial.println(" seconds (timerDelay variable), it will take this many seconds before publishing the first reading.");
+	   
+		}//~ end setup loop   
 
 void loop() {
   //Send an HTTP POST request every 10 minutes
@@ -166,7 +182,7 @@ void loop() {
 
       // Prepare your HTTP POST request data
       String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
-                            + "&location=" + sensLocatArray[0] + "&value1=" + String(bme.readTemperature())
+                            + "&location=" + sensorHome + "&value1=" + String(bme.readTemperature())
                             + "&value2=" + String(bme.readHumidity()) + "&value3=" + String(bme.readPressure()/100.0F) + "";
       Serial.print("httpRequestData: ");
       Serial.println(httpRequestData);
@@ -190,13 +206,14 @@ void loop() {
     }
     lastTime = millis();
   }
-}
+}//~ end loop
+
 // *****************************************************************************
-// void ReadSetupFile(char filename[20]) (myFile)
+// void ReadSetupFile(const char* filename, WifiConfig &wifi_conf)
 // *****************************************************************************
-    void ReadSetupFile(char filename[20]){
+    void ReadSetupFile(const char* filename, WifiConfig &wifi_conf){
       //read network credentials from SD card
-      myFile = SD.open(filename);//"/setup.js");
+      myFile = SD.open(filename);                    //"/setup.js"
       if (!myFile) {
         Serial.println("setup.js file doesn't exist. Save it on SD card and restart.");
         return;
@@ -207,14 +224,8 @@ void loop() {
       const size_t capacity = JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(1) + 3 * JSON_OBJECT_SIZE(2) + 130;
       //DynamicJsonDocument doc(capacity);
       StaticJsonDocument<512> doc;
-    
-      char* json = new char [myFile.size() + 1];
-      myFile.read((uint8_t*)json, myFile.size());
-      json[myFile.size()] = 0;  //make a CString
-      myFile.close();
-    
-      //const char* json = "{\"WifiNetwork\":[{\"ssid\":\"iiNet7F89AA\",\"password\":\"7E3CK5FQJTAY576\"},{\"ssid\":\"officenet\",\"password\":\"admin1\"},{\"ssid\":\"iPhone(Johny)\",\"password\":\"12345\"}]}";
-      DeserializationError  err = deserializeJson(doc, json);
+
+      DeserializationError  err = deserializeJson(doc, myFile);
       switch (err.code())
       {
         case DeserializationError::Ok:
@@ -237,24 +248,25 @@ void loop() {
       Serial.println();
       Serial.printf("number of Wi-Fi networks: %d\r\n", wifi_net_no);
     
-      if (wifi_net_no > 10) {
-        Serial.println("too many WiFi networks on SD card, 10 max");
-        wifi_net_no = 10;
+      if (wifi_net_no > 5) {
+        Serial.println("too many WiFi networks on SD card, 5 max");
+        wifi_net_no = 5;
       }
       for (int i = 0; i < wifi_net_no; i++) {
         String ssid = WifiNetwork[i]["ssid"];
         String password = WifiNetwork[i]["password"];
-        const char* sensorLocation = WifiNetwork[i]["sensorLocation"];          // added Station Location***************
-        strcpy(sensLocatArray[i],sensorLocation);                               // added Station Location***************
+        strcpy(sensSSIDArray[i],WifiNetwork[i]["ssid"]);
+        strcpy(sensPSWRDArray[i],WifiNetwork[i]["password"]);
+        strcpy(sensLocatArray[i],WifiNetwork[i]["sensorLocation"]);                               // added Station Location***************
         
         wifiMulti.addAP(ssid.c_str(), password.c_str());
     
-        //Serial.printf("WiFi %d: SSID: \"%s\" ; PASSWORD: \"%s\" ; LOCATION: \"%s\"\r\n", i, ssid.c_str(), password.c_str(), sensorLocation);
-        Serial.printf("LOCATION: \"%s\"\r\n", i, sensorLocation);
+        Serial.printf("WiFi %d: SSID: \"%s\" ; PASSWORD: \"%s\" ; LOCATION: \"%s\"\r\n", i, ssid.c_str(), password.c_str(), sensorLocation.c_str());
+        //Serial.printf("LOCATION: \"%s\"\r\n", i, sensorLocation.c_str());
       }
-      Serial.print("Device location is: ");
-      Serial.println(sensLocatArray[0]);
-     }
+      myFile.close();
+   } 
+     
  // *****************************************************************************
  // void listDir(fs::FS &fs, const char * dirname, uint8_t levels)
  // *****************************************************************************
@@ -310,4 +322,4 @@ void loop() {
     ]
 }
 */
- */
+
